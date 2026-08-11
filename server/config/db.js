897b01477136b047f77ffafa652730673fab4611
Anish Rathod <anishr6353@gmail.com>
2026-08-11 14:43:37 +0530

    backend complated

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dns from "dns";
+// Force Node.js to use Google DNS for lookups
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
--- server/routes/task.routes.js
+++ server/routes/task.routes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { addTask, getAllTask } from "../controllers/task.controller";
+import {
+  addTask,
+  deleteTask,
+  getAllTask,
+  getOneTask,
+  updateTask,
+} from "../controllers/task.controller.js";
 
 const router = express.Router();
 
 router.post("/add", addTask);
 router.get("/", getAllTask);
+router.get("/:id", getOneTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 export default router;
